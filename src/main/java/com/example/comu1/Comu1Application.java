8e63888bd003420d4aefa7acd7134dd29716e7c9
minchoer144 <minchoer144@gmail.com>
2022-08-09 11:37:24 +0900

    open

---
+++ src/main/java/com/example/comu1/Comu1Application.java
@@ -0,0 +1,13 @@
+package com.example.comu1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comu1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comu1Application.class, args);
+    }
+
+}
